6bbd197bbfb983d3223835a68029019df4a18950
thivy <thivyakanth@gmail.com>
2025-08-09 09:30:49 +1000

    Refactor font settings in globals.css and layout.tsx to use Montserrat font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import { ContentContainer } from "@/components/content-container";
 import { PageHeader } from "@/components/page-header";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
 
@@ -27,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-100`}
+        className={`${montserrat.className} antialiased scroll-smooth bg-slate-100`}
       >
         <PageHeader />
         <ContentContainer>{children}</ContentContainer>
