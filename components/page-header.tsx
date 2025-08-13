09d63c0eb2a2f0f7a5086243b5dc43596d3fd125
thivy <thivyakanth@gmail.com>
2025-08-13 18:05:25 +1000

    Update PageHeader styles for improved readability and adjust HeroCategoryMenu padding for consistency

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -2,13 +2,16 @@ import { ContentContainer } from "./content-container";
 
 export const PageHeader = () => {
   return (
-    <ContentContainer className="py-6 space-y-2">
-      <h1 className="scroll-m-20 text-4xl  tracking-tight text-balance">
-        <span className="font-extrabold">Pixel</span>Prompt
+    <ContentContainer className="py-12 space-y-2">
+      <h1 className="scroll-m-20  text-4xl md:text-6xl font-medium tracking-tight text-balance">
+        <span className="font-extrabold">PIXEL</span>Prompt
       </h1>
-      <p className="text-lg sm:text-2xl text-gray-600 text-balance tracking-tight font-medium">
-        A handpicked gallery of AI-fueled visuals and the prompts that power
-        them.
+      <p className="text-2xl sm:text-4xl md:text-6xl text-slate-400 text-balance tracking-tighter font-medium">
+        A handpicked gallery of{" "}
+        <span className="font-semibold bg-gradient-to-r from-blue-300  to-orange-300 bg-clip-text text-transparent">
+          AI-fueled
+        </span>{" "}
+        visuals and the prompts that power them.
       </p>
     </ContentContainer>
   );
