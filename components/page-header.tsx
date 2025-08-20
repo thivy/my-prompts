38a8944333cc031823078d7850dcb1f443b451c3
thivy <thivyakanth@gmail.com>
2025-08-20 17:12:12 +1000

    Checkpoint from VS Code for coding agent session

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
 import { ContentContainer } from "./content-container";
 
+const Img = ({ src, className }: { src: string; className?: string }) => {
+  return (
+    <span className="">
+      <img
+        src={src}
+        className={`w-20 inline-block hover:scale-105 origin-center transition duration-400 ${className}`}
+      />
+    </span>
+  );
+};
+
 export const PageHeader = () => {
   return (
     <ContentContainer className="pt-12 flex flex-col gap-8">
@@ -8,6 +19,14 @@ export const PageHeader = () => {
         href={"/"}
         className="scroll-m-20  text-4xl md:text-6xl font-medium tracking-tight text-balance"
       >
+        <span className="flex scale-75">
+          <Img src={"/header/P_Baloon.png"} className="w-24" />
+          <Img src={"/header/I_Baloon.png"} />
+          <Img src={"/header/X_Baloon.png"} className="w-27" />
+          <Img src={"/header/E_Baloon.png"} />
+          <Img src={"/header/L_Baloon.png"} className="w-26" />
+          Prompt
+        </span>
         <span className="font-extrabold">PIXEL</span>Prompt
       </Link>
       <p className="text-2xl sm:text-4xl md:text-6xl text-slate-400 sm:text-balance tracking-tighter font-normal">
