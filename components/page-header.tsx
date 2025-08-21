0b68b3485e710c74ef3be5452c8669bba7509937
thivy <thivyakanth@gmail.com>
2025-08-22 08:27:24 +1000

    Implement code changes to enhance functionality and improve performance

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -1,22 +1,21 @@
 import Link from "next/link";
 import { ContentContainer } from "./content-container";
-import { PIXELPrompt } from "./PIXELPrompt";
 
 export const PageHeader = () => {
   return (
-    <ContentContainer className="pt-12 flex flex-col gap-8">
+    <ContentContainer className=" ">
       <Link
         href={"/"}
-        className="scroll-m-20 text-4xl md:text-6xl font-medium tracking-tight text-balance"
+        className="text-stone-400 text-xl py-3 inline-block font-semibold tracking-tight text-balance"
       >
-        <PIXELPrompt />
+        <span className="font-extrabold text-stone-500">PIXEL</span> Prompts
       </Link>
-      <p className="text-2xl sm:text-4xl md:text-6xl text-slate-400 sm:text-balance tracking-tighter font-normal">
-        A handpicked gallery of{" "}
-        <span className="font-semibold bg-gradient-to-r from-blue-300  to-orange-300 bg-clip-text text-transparent">
-          AI-fueled
-        </span>{" "}
-        visuals and the prompts that power them.
+      <span className="px-20">
+        <img src={"/header/hero.png"} alt="Main Logo" className="px-20" />
+      </span>
+      <p className="text-2xl sm:text-4xl md:text-6xl text-stone-500 sm:text-balance tracking-tighter font-normal text-center">
+        A handpicked gallery of AI-fueled visuals and the prompts that power
+        them.
       </p>
     </ContentContainer>
   );
