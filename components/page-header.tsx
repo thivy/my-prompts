7abb26e01706672356bc938637acd166288bd2b2
thivy <thivyakanth@gmail.com>
2025-08-22 08:57:32 +1000

    add image path

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -1,3 +1,4 @@
+import { toImagePath } from "@/lib/content";
 import Link from "next/link";
 import { ContentContainer } from "./content-container";
 
@@ -11,7 +12,11 @@ export const PageHeader = () => {
         <span className="font-extrabold text-stone-500">PIXEL</span> Prompts
       </Link>
       <span className="px-20">
-        <img src={"/header/hero.png"} alt="Main Logo" className="px-20" />
+        <img
+          src={toImagePath("/header/hero.png")}
+          alt="Main Logo"
+          className="px-20"
+        />
       </span>
       <p className="text-2xl sm:text-4xl md:text-6xl text-stone-500 sm:text-balance tracking-tighter font-normal text-center">
         A handpicked gallery of AI-fueled visuals and the prompts that power
