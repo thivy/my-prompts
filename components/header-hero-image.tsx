c42e2279cd02019aa8ba920820d3466aaecddc9b
thivy <thivyakanth@gmail.com>
2025-08-22 21:14:52 +1000

    feat: Add HeaderHeroImage component and update layout; adjust padding in HeroCategoryMenu

--- components/header-hero-image.tsx
+++ components/header-hero-image.tsx
@@ -1,5 +1,4 @@
 import { toImagePath } from "@/lib/content";
-import Link from "next/link";
 import { ContentContainer } from "./content-container";
 
 interface HeaderHeroImageProps {
@@ -7,22 +6,13 @@ interface HeaderHeroImageProps {
   description: string;
 }
 
-export const HeaderHeroImage = ({ imageUrl, description }: HeaderHeroImageProps) => {
+export const HeaderHeroImage = ({
+  imageUrl,
+  description,
+}: HeaderHeroImageProps) => {
   return (
     <ContentContainer className="">
-      <Link
-        href={"/"}
-        className="text-stone-400 text-xl py-3 inline-block font-semibold tracking-tight text-balance"
-      >
-        <span className="font-extrabold text-stone-500">PIXEL</span> Prompts
-      </Link>
-      <span className="">
-        <img
-          src={toImagePath(imageUrl)}
-          alt="Hero Image"
-          className="md:px-20"
-        />
-      </span>
+      <img src={toImagePath(imageUrl)} alt="Hero Image" className="md:px-20" />
       <p className="text-2xl py-8 sm:text-4xl md:text-5xl text-stone-500 sm:text-balance tracking-tight font-normal text-center">
         {description}
       </p>
--- components/page-header.tsx
+++ components/page-header.tsx
@@ -1,4 +1,3 @@
-import { toImagePath } from "@/lib/content";
 import Link from "next/link";
 import { ContentContainer } from "./content-container";
 
@@ -11,17 +10,6 @@ export const PageHeader = () => {
       >
         <span className="font-extrabold text-stone-500">PIXEL</span> Prompts
       </Link>
-      <span className="">
-        <img
-          src={toImagePath("/header/hero.png")}
-          alt="Main Logo"
-          className="md:px-20"
-        />
-      </span>
-      <h1 className="text-2xl py-8 sm:text-4xl md:text-5xl text-stone-500 sm:text-balance tracking-tight font-normal text-center">
-        A handpicked gallery of AI-fueled visuals and the prompts that power
-        them.
-      </h1>
     </ContentContainer>
   );
 };
